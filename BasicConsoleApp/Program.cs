165f0c980dda31cddb21dd54a885c2cda777d52e
Rosur <Samaallen@gmail.com>
2015-05-14 22:51:08 +0100

    FizzBuzz C# 6 program (any number)

--- BasicConsoleApp/Program.cs
+++ BasicConsoleApp/Program.cs
@@ -10,9 +10,32 @@ namespace BasicConsoleApp
     {
         static void Main(string[] args)
         {
-            string xd = "new";
-            Console.WriteLine($"testing {xd}");
-            Console.ReadLine();
-        }
+            Console.WriteLine($"Enter number to count to:{Environment.NewLine}");
+            Boolean vaildNumber = false;
+            int usedNumber = 0;
+            while (!vaildNumber)
+            {
+                string number = Console.ReadLine();                
+                vaildNumber = int.TryParse(number, out usedNumber);
+                if (!vaildNumber)
+                    Console.WriteLine("Invalid number, Re-enter:");
+            }
+            Console.WriteLine(Environment.NewLine);
+            int count = 1;
+            while(count < usedNumber)
+            {
+                if(count % 3 == 0 && count % 5 == 0)
+                    Console.WriteLine("FizBuzz");
+                else if(count % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (count % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(count);
+                count++;
+            }
+
+            Console.ReadLine(); // to stop the program before closing
+        }        
     }
 }
